2762b3b9d1bce2f3cb34430881d6248c4886fb52
S25087 <savor-almighty-why@duck.com>
2024-11-15 18:36:22 -0500

    Created a home component function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1
+      className='text-xl'
+      >
+        Home 
+      </h1>
+    </div>
+  );
+};
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/setup",
+    element: <div>Setup</div>,
+  },
+  {
+    path: "/play",
+    element: <div>Play</div>,
+  },
+]);
+
+
 
 function App() {
   return (
     <div className="App">
+      <RouterProvider 
+      router={router}
+      />
       <h1 className="text-3xl font-bold underline">
         Chess
       </h1>
