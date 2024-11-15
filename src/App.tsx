293a8e838b4178bb88baf10fcfc92a6aac018a1b
S25087 <savor-almighty-why@duck.com>
2024-11-15 13:52:11 -0500

    package.json
    This was an old edit, so I don't remember exactly what the change was.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
+      
       <h1 className="text-3xl font-bold underline">
         Chess
       </h1>
