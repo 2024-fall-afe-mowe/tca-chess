d64d989a0bfa1c9781c0da80647c27703340ea20
S25087 <savor-almighty-why@duck.com>
2024-11-18 19:59:03 -0500

    We have lifted game results state.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,52 @@ import {
 import { Home } from './Home';
 import { Setup } from './Setup';
 import { Play } from './Play';
+import{ GameResult } from './game-results';
+
+const dummyGameResults: GameResult[] = [
+  {
+      startTime: "2024-09-23T15:36:25.123Z"
+      , endTime: "2024-09-23T15:46:25.123Z"
+      , winner: "Chris B"
+      , player: [
+          "Chris B"
+          , "Caden J"
+          , "Peter B"
+          , "Swastik A"
+          , "Tom"
+      ]
+  }
+  , {
+      startTime: "2024-09-23T15:48:25.123Z"
+      , endTime: "2024-09-23T15:50:25.123Z"
+      , winner: "Tom"
+      , player: [
+          "Harry"
+          , "Hermione"
+          , "Ron"
+          , "Tom"
+      ]    
+  }
+  , {
+      startTime: ""
+      , endTime: ""
+      , winner: "Harry"
+      , player: [
+          "Harry"
+          , "Chris B"
+          , "Tom"
+      ]
+  }
+  , {
+      startTime: ""
+      , endTime: ""
+      , winner: "Tom"
+      , player: [
+          "Tom"
+          , "Jack"
+      ]
+  }
+];
 
 const App = () => {
 
@@ -33,6 +79,8 @@ const App = () => {
       />,
     },
   ]);
+
+const [gameResults, setGameResults] = useState(dummyGameResults);
 
   return (
     <div className="App">
