83a9779b38b0f3272a6130af06c45b7441791bcb
S25087 <savor-almighty-why@duck.com>
2024-11-18 22:25:54 -0500

    Got the leaderboard working using dummy data.

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,9 +10,9 @@ export const Home: React.FC<HomeProps> = ({
     leaderboardData
 }) => {
 
-console.log(leaderboardData);
 
-const nav = useNavigate();
+
+    const nav = useNavigate();
 
     return (
         <div>
@@ -23,10 +23,65 @@ const nav = useNavigate();
             </h1>
             <button
                 className="btn btn-primary mb-3"
-                onClick={() => nav("/setup")}     
+                onClick={() => nav("/setup")}
             >
                 Play
             </button>
+            <div
+                className="card bg-base-100 shadow-xl"
+            >
+                <h2
+                    className="card-title"
+                >
+                    Leaderboard
+                </h2>
+                {
+                    leaderboardData.length > 0
+                        ? (
+                            <table
+                            className="table table-zebra"
+                            >
+                                <thead>
+                                   <tr>
+                                <th>W</th>
+                                <th>L</th>
+                                <th>AVG</th>
+                                <th></th>
+                                    </tr> 
+                                </thead>
+                                <tbody>
+                                    {
+                                        leaderboardData.map(x =>(
+                                            <tr
+                                            key={x.name}
+                                            >
+                                            <td>
+                                                {x.wins}
+                                            </td>
+                                            <td>
+                                                {x.losses}
+                                            </td>
+                                            <td>
+                                                {x.avg}
+                                            </td>
+                                            <td>
+                                                {x.name}
+                                            </td>
+                                            </tr>
+                                        ))
+                                    }
+
+                                </tbody>
+                            </table>
+                        )
+                        : (
+                            <p>
+                                Play a game to see the Leaderboard!
+                            </p>
+                        )
+                    
+                }
+            </div>
         </div>
     );
 };
